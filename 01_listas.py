2d3d3881b6f90ed465b2222cecc57c113b1d6626
fidesnin <fidesnin.bedu@gmail.com>
2020-11-21 13:28:16 -0600

    added file to explain lists in python

---
+++ 01_listas.py
@@ -0,0 +1,16 @@
+numeros = [23, 98, 127, 1289, 8912 ]
+
+print(numeros)
+
+#append para agregar elementos a una lista
+
+numero_extra = 33
+numero_extra_dos = 1223
+numero_extra_tres = 89
+
+numeros.append(numero_extra)
+print(numeros)
+
+# usamos pop para eliminar el ultimo valor
+numeros.pop()
+print(numeros)
